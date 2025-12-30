23163f93221d233d391ccc4e600b1b4ad4387635
toomone <thomas.cousin@datadoghq.com>
2025-12-30 14:24:51 +0100

    feat: add Datadog RUM SDK with env-aware configuration

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,27 @@
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { PUBLIC_DD_ENV } from '$env/static/public';
+
+	onMount(() => {
+		if (browser) {
+			import('@datadog/browser-rum').then(({ datadogRum }) => {
+				datadogRum.init({
+					applicationId: 'c109fa30-3a43-4ce1-8679-88ae77369152',
+					clientToken: 'pub38efae98b117027f1be52a7ed202072b',
+					site: 'datadoghq.com',
+					service: 'pricehound',
+					env: PUBLIC_DD_ENV || 'prod',
+					sessionSampleRate: 100,
+					sessionReplaySampleRate: 100,
+					trackBfcacheViews: true,
+					defaultPrivacyLevel: 'allow',
+				});
+			});
+		}
+	});
 </script>
 
 <ModeWatcher />
