0c5bbba4244115aa3fc350d37e6db721fb84bfe3
toomone <thomas.cousin@datadoghq.com>
2026-01-02 19:21:09 +0100

    chore: bump version to 1.2.0

--- frontend/src/lib/version.ts
+++ frontend/src/lib/version.ts
@@ -9,5 +9,5 @@
  * This version is sent to Datadog RUM for tracking and filtering.
  * Update this file when releasing new versions.
  */
-export const APP_VERSION = '1.1.0';
+export const APP_VERSION = '1.2.0';
 
